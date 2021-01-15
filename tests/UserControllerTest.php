5cc43510b92fa57dc1c1a4bbd74f1d89f0d85a32
gediminasnn <neniusgediminas@gmail.com>
2021-01-15 19:11:46 +0200

    Mabye last update

--- tests/UserControllerTest.php
+++ tests/UserControllerTest.php
@@ -10,7 +10,13 @@ class UserControllerTest extends WebTestCase
     public function testUser()
     {
         $client = self::createClient();
-        $client->request('POST', '/api/users');
+        $client->request('POST', '/api/users', ['json' => [
+            'users' => [
+                ['first_name'=>'Tom','last_name'=>'Hardy'],
+                ['first_name'=>'Tom','last_name'=>'Gnarly']
+            ]
+        ]
+        ]);
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
 }
